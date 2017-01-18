36e50320c76ab67dc87e95c1cbe537dace2e0654
tom <tom@daoshenggroup.com>
2017-01-18 17:21:02 +0800

    create view

---
+++ uics-ippower-service-provider/src/main/resources/sql/mysql/view.sql
@@ -0,0 +1,32 @@
+create  OR REPLACE view v_element
+AS
+
+
+select id, CONCAT(target_type , '-' , device_ip,'-',device_type,'-',dp_paramname) as target_id, target_type, target_name,device_ip,device_name,device_type,CONCAT(device_ip , "-" , device_type, '-', dp_paramname) as device_id,dt_rtdata_table,dp_paramname from (
+  select Dp_index as id,
+    case  when LOCATE('envihumi',dp_paramname)=1 then 'HUMIDITY'
+          when LOCATE('envitemp',dp_paramname)=1 then 'TEMPERATURE'
+    END as target_type,
+
+    dp_paramdesc as target_name,
+
+    case  when LOCATE('envihumi',dp_paramname)=1 then 'Room_humiture'
+    when LOCATE('envitemp',dp_paramname)=1 then 'Room_humiture'
+    END as device_type,
+
+    pdi_name as device_name, pdi_ip as device_ip,pdi_index,dt_rtdata_table,dp_paramname
+
+  from
+    t_prideviceinfo di ,
+    t_devicetype dt ,
+    t_deviceparam dp
+
+  where
+    di.dpt_id = dt.dt_index
+    and
+    dt.dt_typeid=dp.dt_typeid
+) as target_view
+
+
+
+
