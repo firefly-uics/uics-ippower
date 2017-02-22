373fb66e9e559183036cb326ced3cdbc7756c1f2
tom <tom@daoshenggroup.com>
2017-02-22 16:20:25 +0800

    modify view sql

--- uics-ippower-service-provider/src/main/resources/sql/mysql/view.sql
+++ uics-ippower-service-provider/src/main/resources/sql/mysql/view.sql
@@ -1,9 +1,6 @@
-create  OR REPLACE view v_element
+create or replace view target_view
 AS
-
-
-select id, CONCAT(target_type , '-' , device_ip,'-',device_type,'-',dp_paramname) as target_id, target_type, target_name,device_ip,device_name,device_type,CONCAT(device_ip , "-" , device_type, '-', dp_paramname) as device_id,dt_rtdata_table,dp_paramname from (
-  select Dp_index as id,
+select Dp_index as id,
     case  when LOCATE('envihumi',dp_paramname)=1 then 'HUMIDITY'
           when LOCATE('envitemp',dp_paramname)=1 then 'TEMPERATURE'
     END as target_type,
@@ -25,7 +22,13 @@ select id, CONCAT(target_type , '-' , device_ip,'-',device_type,'-',dp_paramname
     di.dpt_id = dt.dt_index
     and
     dt.dt_typeid=dp.dt_typeid
-) as target_view
+    ;
+
+
+create  OR REPLACE view v_element
+AS
+select id, CONCAT(target_type , '-' , device_ip,'-',device_type,'-',dp_paramname) as target_id, target_type, target_name,device_ip,device_name,device_type,CONCAT(device_ip , "-" , device_type, '-', dp_paramname) as device_id,dt_rtdata_table,dp_paramname
+from target_view;
 
 
 
